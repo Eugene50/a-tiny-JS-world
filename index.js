a9c106fcfb94c55c79c8f25ceb28bbdfef638c9a
unknown <nova_alf@ukr.net>
2018-12-12 15:59:00 +0200

    update code

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+class Citizens {
+    constructor(species, gender, name, lags ='', hands = '', say){
+     this.species = species;
+     this.gender = gender;
+     this.name = name;
+     this.lags = lags;
+     this.hands = hands;
+     this.say = say;   
+    }
+};
 
 
+ let dog = new Citizens('dog','male','Sharik',2, 4,'Gaf!!!');
+ let cat = new Citizens('cat', 'male', 'Murzik', 4, 0,'Mjau)' );
+ let man = new Citizens('human','male','Alex','2','2','Hello to All!!!');
+ let woman = new Citizens('human','female','Maria','2','2','I want coffee!!!');
+
+print(`${dog.species}; ${dog.gender}; ${dog.name};  ${dog.lags}; ${dog.hands}; ${dog.say} `);
+print(`${cat.species}; ${cat.gender}; ${cat.name};  ${cat.lags}; ${cat.hands}; ${cat.say} `);
+print(`${man.species}; ${man.gender}; ${man.name};  ${man.lags}; ${man.hands}; ${man.say} `);
+print(`${woman.species}; ${woman.gender}; ${woman.name};  ${woman.lags}; ${woman.hands}; ${woman.say} `);
